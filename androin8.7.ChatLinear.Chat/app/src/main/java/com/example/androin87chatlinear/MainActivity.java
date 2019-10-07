d536c10926446da02eb446907da185e6465785ec
Rinat Sagitov <yawarwar@gmail.com>
2019-10-08 03:39:41 +0500

    Update 8 module add ChatLinear 8.7 homework

---
+++ androin8.7.ChatLinear.Chat/app/src/main/java/com/example/androin87chatlinear/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.androin87chatlinear;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.example.androin87chatlinear.R;
+
+public class MainActivity extends AppCompatActivity {
+    Spinner spinnerUsers;
+    Spinner spinnerGroups;
+    TextView textViewUser;
+    TextView textViewGroup;
+    TextView textViewMessage;
+    EditText editTextMessage;
+    Button buttonSendMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinnerUsers = findViewById(R.id.spinnerUsers);
+        spinnerGroups = findViewById(R.id.spinnerGroups);
+        textViewUser = findViewById(R.id.textViewUser);
+        textViewGroup = findViewById(R.id.textViewGroup);
+        textViewMessage = findViewById(R.id.textViewMessage);
+        editTextMessage = findViewById(R.id.editTextMessage);
+        buttonSendMessage = findViewById(R.id.buttonSendMessage);
+    }
+
+    public void sendMessage(View view) {
+        textViewMessage.setText(editTextMessage.getText());
+        textViewUser.setText(spinnerUsers.getSelectedItem().toString());
+        textViewGroup.setText(spinnerGroups.getSelectedItem().toString());
+
+    }
+}
